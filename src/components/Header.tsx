5cbfeb1fc256a54b3266db54d80e3215d2f5f759
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-15 15:31:19 +0200

    test font and basic tailwind class

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
-
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="font-[Codystar] text-4xl md:text-7xl text-black container mx-auto text-center">
+      Bloggy
+    </div>
+  );
 }
