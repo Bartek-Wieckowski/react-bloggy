05842080b4b4c792ab8eb6935e74dbf2d1d81e2e
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-17 09:35:36 +0200

    used only Header component

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import Home from "./pages/Home";
 // components
 import Top from "./components/Top";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <Top>
         <Header />
-        <Navbar />
       </Top>
       <Routes>
         <Route path="/" element={<Home />} />
