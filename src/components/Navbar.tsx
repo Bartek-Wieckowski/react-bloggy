53e8f61404b3c49d5ec1468208f759e3f5ad65c0
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-18 12:05:06 +0200

    created Navbar component and used Link from react-router

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,19 @@
-
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="mt-4 border-y-2 border-gray-800">
+      <ul className="flex justify-center items-center gap-4 py-4">
+        <li>
+          <Link to="/">BLOG</Link>
+        </li>
+
+        <li>
+          <Link to="/login">LOGIN </Link>
+        </li>
+        <li>
+          <Link to="/register">REGISTER</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
