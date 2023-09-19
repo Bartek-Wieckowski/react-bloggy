77a64ef414dcffbe1920f3b875225e22bec59f9b
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-19 16:08:02 +0200

    used ThemeProvider and added Main component in all routes pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,25 @@ import Register from "./pages/Register";
 // components
 import Top from "./components/Top";
 import Header from "./components/Header";
+import Main from "./components/Main";
+// context
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <Top>
-        <Header />
-      </Top>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <ThemeProvider>
+        <Top>
+          <Header />
+        </Top>
+        <Main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Main>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
